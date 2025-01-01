7a1e88ea61c53a8fde8144b8bdcc8062a4aac1f9
colonel-lp <col28282@gmail.com>
2025-01-01 19:09:45 +0000

    Update pico-json.py

--- pico-json.py
+++ pico-json.py
@@ -287,7 +287,6 @@ def readBatt(sensorId, elementId):
         if (timeRemaining < 0):
             timeRemaining = 60 * 60 * 24 * 7  # One week
         sensorListTmp[sensorId].update({'capacity.timeRemaining': timeRemaining})
-    sensorListTmp[sensorId]['capacity.nominal'] = sensorListTmp[sensorId]['capacity.nominal'] / 43200
 
 def readBattNameVoltage(sensorId, elementId):
     voltage = element[elementId + 2][1] / float(1000)
