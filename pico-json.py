19d1349f770e2c5b25d85d5fa3fb16c4dcd4ab7d
colonel-lp <col28282@gmail.com>
2025-01-01 19:35:19 +0000

    Update pico-json.py

--- pico-json.py
+++ pico-json.py
@@ -262,8 +262,8 @@ def readTemp(sensorId, elementId):
 
 def readTank(sensorId, elementId):
     currentLevel = element[elementId][0] / float(1000)
-    capacity = sensorList[sensorId].get('capacity', 0) * 1000
-    remainingCapacity = element[elementId][1] / float(10000)
+    capacity = sensorList[sensorId].get('capacity', 0)
+    remainingCapacity = element[elementId][1] / float(10)
     percentage = (remainingCapacity / capacity) * 100 if capacity else 0
     sensorListTmp[sensorId].update({'currentLevel': currentLevel})
     sensorListTmp[sensorId].update({'remainingCapacity': remainingCapacity})
