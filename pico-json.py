b7fe46f3902310ea82df6b2c96431924279a3ee7
colonel-lp <col28282@gmail.com>
2025-01-02 21:11:25 +0000

    Update pico-json.py

--- pico-json.py
+++ pico-json.py
@@ -223,10 +223,10 @@ def createSensorList(config):
         if (type == 13):
             type = 'inclinometer'
             sensorList[id].update({'name': config[entry][3]})
-            inclinometer_type = config[entry][2][2]
+            inclinometer_type = config[entry][3][1]
             sensorList[id].update({'inclinometer_type': inclinometer_type})
             elementSize = 1
-            print(f"Inclinometer ID: {id}, Name: {config[entry][3]}, Type: {inclinometer_type}")
+            print(f"Inclinometer ID: {id}, Name: {config[entry][3][1]}, Type: {inclinometer_type}")
 
         sensorList[id].update({'type': type, 'pos': elementPos})
         elementPos = elementPos + elementSize
