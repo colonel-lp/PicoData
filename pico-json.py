fc9171a05a24faa0b2124237331e7e3231dc4a38
colonel-lp <col28282@gmail.com>
2025-01-02 21:35:55 +0000

    Update pico-json.py

--- pico-json.py
+++ pico-json.py
@@ -308,7 +308,6 @@ def readCurrent(sensorId, elementId):
 def readIncline(sensorId, elementId):
     degree = element[elementId][1] / 10.0
     sensorListTmp[sensorId].update({'degree': degree})
-    print(f"degree: {element[elementId][1]}")
     
 while True:
     updates = []
@@ -390,11 +389,10 @@ while True:
                     if sensorData['type'] == 'barometer':
                         output["barometer"] = filtered_values["pressure"]
                     elif sensorData['type'] == 'inclinometer':
-                        if "degree" in filtered_values:
-                            if filtered_values["inclinometer_type"] == 1:
-                                output["inclinometer"]["pitch"] = filtered_values["degree"]
-                            elif filtered_values["inclinometer_type"] == 2:
-                                output["inclinometer"]["roll"] = filtered_values["degree"]
+                        if filtered_values["inclinometer_type"] == 1:
+                             output["inclinometer"]["pitch"] = filtered_values["degree"]
+                        elif filtered_values["inclinometer_type"] == 2:
+                             output["inclinometer"]["roll"] = filtered_values["degree"]
                     elif sensorData['type'] == 'volt':
                         output["voltage"][name] = filtered_values["voltage"]
                     elif sensorData['type'] == 'current':
