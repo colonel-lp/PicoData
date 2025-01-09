0dc92e97bfd11d40f818bed819cc217e5c04a57c
colonel-lp <col28282@gmail.com>
2025-01-09 17:22:43 +0000

    Update pico.py

--- pico.py
+++ pico.py
@@ -336,9 +336,9 @@ def readCurrent (sensorId, elementId):
 def readIncline(sensorId, elementId):
     degree = element[elementId][1]
     if (degree > 600):
-        degree = (65535 - degree) / -10
+        degree = (65535 - degree) / 10
     else:
-        degree = degree / 10
+        degree = degree / -10
     sensorListTmp[sensorId].update({'degree': degree})
 
 while True:
